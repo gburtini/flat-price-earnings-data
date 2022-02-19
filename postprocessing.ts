5f4953647c0f7106f5213d9b48ae4689f2e9092a
Giuseppe Burtini <giuseppe@burtini.ca>
2022-02-19 11:55:26 -0800

    Fixing postprocessing.ts.

--- postprocessing.ts
+++ postprocessing.ts
@@ -1,30 +1,46 @@
-import { writeJSON, readTXT } from "https://deno.land/x/flat@0.0.14/mod.ts";
+import {
+  writeJSON,
+  readTXT,
+  removeFile,
+} from "https://deno.land/x/flat@0.0.14/mod.ts";
 import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
 
 const outputFilename = "./data.json";
-const inputFileName = "./spx-pe.html";
+const inputFileName = Deno.args[0];
 
 try {
-  const html = readTXT(inputFileName);
-  const $ = cheerio.load(html);
+  const html = await readTXT(inputFileName);
+  const $ = await cheerio.load(html);
 
   const rows = $("#datatable tr");
-  const data = rows.each((_, row) => {
-    const [left, right] = $(row).find("td");
-    const note = $($(right).find("span")[0]).text();
-    $(right).find("span").remove();
-    const value = $(right).text();
+  const data = rows
+    .map((i, row) => {
+      if (i === 0) return;
 
-    return {
-      date: $(left).text(),
-      value,
-      note,
-    };
-  });
+      const columns = $(row).find("td");
+      const left = columns[0];
+      const right = columns[1];
 
-  console.log(data.length);
+      const note = $($(right).find("span")[0]).text();
+      $(right).find("span").remove();
+
+      const value = $(right).text().trim();
+
+      const dateFormatted = new Date($(left).text())
+        .toISOString()
+        .split("T")[0];
+      return {
+        date: dateFormatted,
+        value: parseFloat(value),
+        note,
+      };
+    })
+    .get();
+
+  console.log("Successfully processed", data.length, "rows.");
 
   await writeJSON(outputFilename, data);
+  await removeFile(inputFileName);
 } catch (error) {
   console.log(error);
 }
